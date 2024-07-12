c891be9e8b6327e151db47eb2ba2540dd2dd28b7
jeraldin2003 <jeraldin2003@gmail.com>
2024-07-12 12:50:07 +0530

    first commit

--- gms/gym_management_system/doctype/trainer_feedback/trainer_feedback.py
+++ gms/gym_management_system/doctype/trainer_feedback/trainer_feedback.py
@@ -2,8 +2,49 @@
 # For license information, please see license.txt
 # 
 
-#import frappe
+import frappe
 from frappe.model.document import Document
 
 class TrainerFeedback(Document):
-    pass
+    def before_save(self):
+        ###coding logic for calculating ratinglatest_doc= frappe.get_last_doc('Trainer Feedback')
+        latest_doc= frappe.get_last_doc('Trainer Feedback')
+        doc_data = frappe.db.get_value('Trainer Feedback',latest_doc,'trainer')
+        #print("\n\n\n",doc_data,"\n\n\n\n\n")
+      
+        trainers = frappe.get_all('Trainers', fields=['name','rating'])
+        #print("\n\n\n\n\n\n",trainers )
+        trainer_feedback = frappe.db.get_all('Trainer Feedback',fields=['trainer','rating'])
+        trainer=doc_data
+        for trainer in trainers:
+            c=0
+            a=0
+            x=0
+            for feedback in trainer_feedback:
+                if feedback['trainer'] == trainer['name']:
+                    y=feedback['rating']
+                    x=x+y
+                    c=c+1
+            a=x/c
+            b=trainer['name']
+            print("\n\n\n\n",b,"\n\n\n\n\n")
+            print(a,trainer['name'])
+            #print('hellooooooooooooooooooooooooo\n\n\n\n\n\n\n')
+            #print(trainer,'\n\n\n\n\n')
+
+
+
+        ###use frappe.set_value to insert the value to particular field 
+    
+
+            frappe.db.set_value('Trainers',b,'rating',a)
+    
+    
+    
+##############################################################------------------------------------------------------------
+    #def before_save(self):
+     #   latest_doc = frappe.get_last_doc('Trainer Feedback')
+      #  print(latest_doc,"\n\n\n\n")
+
+#def get_latest_feedback():
+ #   return frappe.get_last_doc('Trainer Feedback')
--- gms/gym_management_system/doctype/trainers/trainers.py
+++ gms/gym_management_system/doctype/trainers/trainers.py
@@ -4,16 +4,17 @@
 import frappe
 from frappe.model.document import Document 
 
-
 class Trainers(Document):
        def before_save(self):
-        # frappe.throw('testing')
-        print ('\n\n\n\n')
+
+        latest_doc= frappe.get_last_doc('Trainer Feedback')
+        doc_data = frappe.db.get_value('Trainer Feedback',latest_doc,'trainer')
+        #print("\n\n\n",doc_data,"\n\n\n\n\n")
+      
         trainers = frappe.get_all('Trainers', fields=['name','rating'])
+        #print("\n\n\n\n\n\n",trainers )
         trainer_feedback = frappe.db.get_all('Trainer Feedback',fields=['trainer','rating'])
-        print ("FEED BACK +======/n\n",trainer_feedback)
-        print ("TRAINER====",trainers)
-        
+        trainer=doc_data
         for trainer in trainers:
             c=0
             a=0
@@ -25,12 +26,16 @@ class Trainers(Document):
                     c=c+1
             a=x/c
             b=trainer['name']
+            print("\n\n\n\n",b,"\n\n\n\n\n")
             print(a,trainer['name'])
-            print('hellooooooooooooooooooooooooo\n\n\n\n\n\n\n')
-            doc=trainer
-            doc.rating = a
+            #print('hellooooooooooooooooooooooooo\n\n\n\n\n\n\n')
+            #print(trainer,'\n\n\n\n\n')
+
+            frappe.db.set_value('Trainers','Ron-Loads-13','rating','100')
+            frappe.db.commit()
             
-        
+
+
 # def before_save(self):
 # 		self.full_name = self.first_name + " " + self.last_name
 		
