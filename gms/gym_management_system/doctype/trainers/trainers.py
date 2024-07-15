0c37ed434d8320d134bcb7f7f63067910b2d2654
jeraldin2003 <jeraldin2003@gmail.com>
2024-07-15 11:24:34 +0530

    new commit

--- gms/gym_management_system/doctype/trainers/trainers.py
+++ gms/gym_management_system/doctype/trainers/trainers.py
@@ -5,35 +5,8 @@ import frappe
 from frappe.model.document import Document 
 
 class Trainers(Document):
-       def before_save(self):
-
-        latest_doc= frappe.get_last_doc('Trainer Feedback')
-        doc_data = frappe.db.get_value('Trainer Feedback',latest_doc,'trainer')
-        #print("\n\n\n",doc_data,"\n\n\n\n\n")
-      
-        trainers = frappe.get_all('Trainers', fields=['name','rating'])
-        #print("\n\n\n\n\n\n",trainers )
-        trainer_feedback = frappe.db.get_all('Trainer Feedback',fields=['trainer','rating'])
-        trainer=doc_data
-        for trainer in trainers:
-            c=0
-            a=0
-            x=0
-            for feedback in trainer_feedback:
-                if feedback['trainer'] == trainer['name']:
-                    y=feedback['rating']
-                    x=x+y
-                    c=c+1
-            a=x/c
-            b=trainer['name']
-            print("\n\n\n\n",b,"\n\n\n\n\n")
-            print(a,trainer['name'])
-            #print('hellooooooooooooooooooooooooo\n\n\n\n\n\n\n')
-            #print(trainer,'\n\n\n\n\n')
-
-            frappe.db.set_value('Trainers','Ron-Loads-13','rating','100')
-            frappe.db.commit()
-            
+    pass
+       
 
 
 # def before_save(self):
