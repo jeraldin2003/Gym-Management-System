cd0062051f0bb2b4e0c43479ca7f214ba2af3dd1
jeraldin2003 <jeraldin2003@gmail.com>
2024-07-19 17:36:56 +0530

    commit again

--- gms/gym_management_system/doctype/invoice/invoice.py
+++ gms/gym_management_system/doctype/invoice/invoice.py
@@ -7,6 +7,7 @@ from frappe.model.document import Document
 class Invoice(Document):
 	def before_save(self):
 
+		
 
 		################################################### Code for Classes attend and Class costs
 		all_bookings= frappe.db.get_all('Class Booking',fields=['member_id','cost'])
@@ -27,6 +28,20 @@ class Invoice(Document):
 				self.locker_occupied = lockers['member_id']
 		
 
-		#############################################
+		##################################################
+		x=int(self.total_expense_this_month)+int(self.locker_cost)+int(self.membership_expenses)
+		y=((int(self.total_expense_this_month)+int(self.locker_cost)+int(self.membership_expenses))*18)/100
+		self.append("table_fcac", {
+    			"total_expense_this_month": self.total_expense_this_month,
+    			"cost_of_lockers": self.locker_cost,
+				"membership_expense": self.membership_expenses,
+				"grand_total": x,
+				"taxes_18": y,
+				"net_total": x+y,
+				"classes_attended": self.classes_attended,
+				"locker_occupied": self.locker_occupied,
+				"membership": self.membership,})
 
 		
+		
+
